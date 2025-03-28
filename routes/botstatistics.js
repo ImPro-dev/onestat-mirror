224a1f600f142648fe3fd3e1d16e96d0cb43a63d
Igor Mazurenko <igor.mazurenko@ewave.com>
2025-03-28 14:04:44 +0200

    ONST-0008: Multilanguage for FB files added

--- routes/botstatistics.js
+++ routes/botstatistics.js
@@ -128,11 +128,9 @@ router.post('/',
         // FB data
         var adname, adIndex, adFound;
         var spend, spendIndex, spendFound;
-        var adColumnName = config.FB.adColumnName;
-        var adColumnName_ua = config.FB.adColumnName_ua;
+        var adColumnNameRegex = config.FB.adColumnNameRegex;
+        var spendColumnNameRegex = config.FB.spendColumnNameRegex;
         var spendColumnName = config.FB.spendColumnName;
-        var spendRegex = config.FB.spendRegex;
-        var spendRegex_ua = config.FB.spendRegex_ua;
 
         // var FBreader = fs.createReadStream("uploads/___FB.csv");
         var FBreader = fs.createReadStream(path.join(__dirname, '..', 'uploads', webID, "___FB.csv"));
@@ -142,12 +140,11 @@ router.post('/',
             if (FBcvsRow == 1) {
               row.forEach((columnName, index) => {
                 if (!adFound) {
-                  adIndex = (columnName.toLowerCase() === adColumnName.toLowerCase()
-                    || columnName.toLowerCase() === adColumnName_ua.toLowerCase()) ? index : '';
+                  adIndex = adColumnNameRegex.test(columnName) ? index : '';
                   adFound = adIndex === '' ? false : true;
                 }
                 if (!spendFound) {
-                  spendIndex = (spendRegex.test(columnName) || spendRegex_ua.test(columnName)) ? index : '';
+                  spendIndex = spendColumnNameRegex.test(columnName) ? index : '';
                   spendFound = spendIndex === '' ? false : true;
                 }
               });
